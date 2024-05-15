338c13cf4b0607c2085f00d5908b9f817237f727
Ruslan <118926729+Tunchichi@users.noreply.github.com>
2024-05-16 00:26:10 +0300

    Update rat.py

--- rat.py
+++ rat.py
@@ -2,3 +2,11 @@
 
 from .. import loader, utils
 from asyncio import sleep
+
+
+@loader.tds
+  class ratMod(loader.Module):
+	  """rat"""
+	  strings = {'name': 'rat'}
+	@loader.owner
+	async def ratcmd(self, message_id):
